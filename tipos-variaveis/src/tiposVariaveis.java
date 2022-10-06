24f107e91fe33f8da87df7ee4c85ea17e900ccbe
David Santana <davidfjobs@gmail.com>
2022-10-06 15:37:02 -0300

    commit final

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -1,5 +1,17 @@
 public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!!!");
+        double salarioMinino = 2500.00;
+
+        short numeroCurto = 1;
+        int numeroNormal =  numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
