a55b019b98d2ccb614ff0d35651dad8bca630b8e
David Santana <davidfjobs@gmail.com>
2022-10-06 16:06:52 -0300

    commit tal

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+    public static void main(String [] args){
+       int a = 5;
+       int b = 6;
+
+       String resultado =  a==b ?"verdadeira":"falsa";
+
+       
+    }
+}
