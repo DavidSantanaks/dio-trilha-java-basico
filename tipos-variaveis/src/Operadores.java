d95c37bff8b1104b4e86ba64cc19d1e85ce5b4ed
David Santana <davidfjobs@gmail.com>
2022-10-06 16:25:13 -0300

    Leão de git

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -2,7 +2,7 @@ public class Operadores {
     public static void main(String [] args){
        int a = 5;
        int b = 6;
-
+        //operador ternario
        String resultado =  a==b ?"verdadeira":"falsa";
 
        
